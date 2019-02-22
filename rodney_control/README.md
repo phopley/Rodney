3a54ce1d73d2b57469b01eefac3c9c8237b0da27
phopley <philip@thehopleys.me.uk>
2019-02-22 13:57:16 +0000

    No LIDAR but can configure for Teensy or Arduino Nano

--- rodney_control/README.md
+++ rodney_control/README.md
@@ -2,23 +2,16 @@
 This Arduino/Teensy sketch, rodney_control, runs using rosserial and is a ROS node which:
 * Controls upto four RC Servos on the servo topic
 * Publishes the tacho on the tacho topic monitoring two motors with Hall sensors
-* Controls a servo attached to a LIDAR and takes range measurments from the LIDAR publishing them on the laserScan topic
 
 The node subscribes to the servo topic and acts on a rodney_msgs::servo_array message.
 This message contains two elements, index and angle. Index references the servos 0-3 and
 angle is the angle to set the servo to 0-180.
 
-The node also subscribes to the /commands/lidar_enable topic the acts on a std_msgs::Bool message.
-If the data in the message is true the use of the LIDAR is enabled, otherwise it is disabled.
-
 The connections to a Teensy 3.5 are:
 * Pin 0 (INT)   -> used for monitoring right motor speed
-* Pin 1 (Input)   -> used for sensing right motor direction
-* Pin 2 (PWM)   -> LIDAR Servo control signal 
+* Pin 1 (Input)   -> used for sensing right motor direction 
 * Pin 3 (INT)   -> used for monitoring left motor speed
 * Pin 4 (Input) -> used for sensing left motor direction 
-* Pin 18 (SCL)  -> LIDAR SCL (Green wire on Garmin LIDAR-Lite V3)
-* Pin 19 (SDA)  -> LIDAR SDA (Blue wire on Garmin LIDAR-Lite V3)
 * Pin 20 (PWM)  -> servo indexed 3
 * Pin 21 (PWM)  -> servo indexed 2 
 * Pin 22 (PWM)  -> servo indexed 1
--- rodney_control/rodney_control.ino
+++ rodney_control/rodney_control.ino
@@ -2,45 +2,53 @@
  * This version: 
  * - Controls upto four RC Servos on the servo topic
  * - Publishes the tacho on the tacho topic monitoring two motors with Hall sensors.
- * - Controls a servo attached to a LIDAR and takes range measurments from the LIDAR publishing them on the laserScan topic
  * 
  * The node subscribes to the servo topic and acts on a rodney_msgs::servo_array message.
  * This message contains two elements, index and angle. Index references the servos 0-3 and 
  * angle is the angle to set the servo to 0-180.
  * 
- * The node also subscribes to the /commands/lidar_enable topic the acts on a std_msgs::Bool message.
- * If the data in the message is true the use of the LIDAR is enabled, otherwise it is disabled. 
  *
  * The connections to a Teensy 3.5 are:  
  * Pin 0 (INT)   -> used for monitoring right motor speed
- * Pin 1 (Input)   -> used for sensing right motor direction
- * Pin 2 (PWM)   -> LIDAR Servo control signal 
+ * Pin 1 (Input) -> used for sensing right motor direction
  * Pin 3 (INT)   -> used for monitoring left motor speed
  * Pin 4 (Input) -> used for sensing left motor direction 
- * Pin 18 (SCL)  -> LIDAR SCL (Green wire on Garmin LIDAR-Lite V3)
- * Pin 19 (SDA)  -> LIDAR SDA (Blue wire on Garmin LIDAR-Lite V3)
  * Pin 20 (PWM)  -> servo indexed 3
  * Pin 21 (PWM)  -> servo indexed 2 
  * Pin 22 (PWM)  -> servo indexed 1
- * PIN 23 (PWM)  -> servo indexed 0
+ * PIN 23 (PWM)  -> servo indexed 0 
+ * 
+ * The connections to a Nano are:
+ * D2 (INT)   -> used for monitoring right motor speed
+ * D4 (Input) -> used for sensing right motor direction
+ * D3 (INT)   -> used for monitoring left motor speed
+ * D7 (Input) -> used for sensing left motor direction 
+ * D10 (PWM)  -> servo indexed 3
+ * D5 (PWM)  -> servo indexed 2 
+ * D6 (PWM)  -> servo indexed 1
+ * D9 (PWM)  -> servo indexed 0  
  * 
  */
-
-#include <PWMServo.h>
-#include <LIDARLite.h>
-#include "ros.h"  // USE "ros.h" not <ros.h> SO THAT WE USE OUR LOCAL VERSION
-#include <sensor_msgs/LaserScan.h>
+#if defined(__MK64FX512__)
+#include <PWMServo.h> // Use PWMServo if using a Teensy
+#include "ros.h"      // Use "ros.h" not <ros.h> so that by using our local version we can increase buffer size if required
+#else
+#include <Servo.h>
+#include <ros.h>
+#endif
 #include <servo_msgs/servo_array.h>
 #include <tacho_msgs/tacho.h>
-#include <std_msgs/Bool.h>
 
 void servo_cb( const servo_msgs::servo_array& cmd_msg);
-void command_lidar_cb( const std_msgs::Bool& cmd_msg);
 void WheelSpeed0();
 void WheelSpeed1();
 
-#define SERVO_LIDAR 2 // LIDAR Servo
+#define LED_PIN 13  // Onboard LED
 
+#define GEAR_BOX_COUNTS_PER_REV 1440.0f
+
+#if defined(__MK64FX512__) 
+// Teensy
 // Define the PWM pins that the other servos are connected to
 #define SERVO_0 23
 #define SERVO_1 22
@@ -53,39 +61,36 @@ void WheelSpeed1();
 #define ENCODER1_PINA 3  // Interrupt
 #define ENCODER1_PINB 4  // Digital pin
 
-#define LED_PIN 13  // Onboard LED
-
-#define GEAR_BOX_COUNTS_PER_REV 1440.0f
-
-#define NUM_READINGS 181 // 1 each degree (0 to 180)
-#define TRUE_MIN_ANGLE (-1.571)
-#define TRUE_MAX_ANGLE (1.571)
-#define ANGLE_INCREMENT (3.142 / NUM_READINGS)
-
-PWMServo  servoLidar;
 PWMServo  servo0;
 PWMServo  servo1;
 PWMServo  servo2;
 PWMServo  servo3;
-LIDARLite myLidarLite;
+#else
+// Define the PWM pins that the other servos are connected to
+#define SERVO_0 9
+#define SERVO_1 9
+#define SERVO_2 5
+#define SERVO_3 10
+
+// Define pins used for two Hall sensors
+#define ENCODER0_PINA 2  // Interrupt 0
+#define ENCODER0_PINB 4  // Digital pin
+#define ENCODER1_PINA 3  // Interrupt 1
+#define ENCODER1_PINB 7  // Digital pin
+
+Servo  servo0;
+Servo  servo1;
+Servo  servo2;
+Servo  servo3;
+#endif
 
 tacho_msgs::tacho      tachoMsg;
-sensor_msgs::LaserScan lidarMsg;
 
 ros::NodeHandle nh;
 
-ros::Publisher lidarPub("/laser_scan", &lidarMsg);
 ros::Publisher tachoPub("tacho", &tachoMsg);
 ros::Subscriber<servo_msgs::servo_array> subServo("servo", servo_cb);
-ros::Subscriber<std_msgs::Bool> subLidarCmd("/commands/lidar_enable", command_lidar_cb);
 
-float deltaTime;
-float ranges[NUM_READINGS];
-bool  laserOn;        // True when the LIDAR is enabled
-bool  incDir;         // True when direction of LIDAR servo is increasing angle
-bool  newScan;        // True when the start of a new scanning sequence is due
-bool  watingOnScan;   // When true have set timer between single scans and waiting for a signle scan to complete
-int   scanIndex;      // Current scan position gives servo position and array position for ranges
 byte  encoder0PinALast;
 byte  encoder1PinALast;
 volatile int encoder0Count; // Number of pulses
@@ -95,25 +100,15 @@ volatile boolean encoder1Direction; //Rotation direction
 unsigned long publisherTime;
 unsigned long currentTime;
 unsigned long lastTime;
-unsigned long scanTime;
 
 
 void setup() 
 {
   nh.initNode();
-  nh.advertise(lidarPub);
   nh.advertise(tachoPub);
   nh.subscribe(subServo);
-  nh.subscribe(subLidarCmd);
 
-  // Attach LIDAR  servo.
-  servoLidar.attach(SERVO_LIDAR);
-  // Start at 0 degrees (-1.571  radians for ROS)
-  // Except the HiTEC HS-422 servo works in the left hand rule not 
-  // the right hand rule used by ros. So set 180 degrees for thsi type of servo
-  servoLidar.write(180); 
-
-  // Attach other servos
+  // Attach servos
   servo0.attach(SERVO_0); //attach it to the pin
   servo1.attach(SERVO_1);
   servo2.attach(SERVO_2);
@@ -134,27 +129,6 @@ void setup()
   // Attach the interrupts for the Hall sensors
   attachInterrupt(digitalPinToInterrupt(ENCODER0_PINA), WheelSpeed0, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENCODER1_PINA), WheelSpeed1, CHANGE);
-    
-  myLidarLite.begin(0, true);
-  myLidarLite.configure(0);
-
-  // To determine the end of the array each one has an auto-generated 
-  // integer companion with the same name and the suffix _length
-  lidarMsg.ranges_length = NUM_READINGS;
-  lidarMsg.intensities_length = 0;
-  lidarMsg.header.frame_id = "lidar_link";
-  lidarMsg.range_min = 0.0;
-  lidarMsg.range_max = 40.0;
-  // These will change sign when the servo moves in the opposite direction
-  lidarMsg.angle_min = TRUE_MIN_ANGLE;
-  lidarMsg.angle_max = TRUE_MAX_ANGLE;
-  lidarMsg.angle_increment = ANGLE_INCREMENT;
-
-  scanIndex = 0;
-  incDir = true;
-  newScan = true;
-  laserOn = false;
-  watingOnScan = false;
 
   // Turn on the onboard LED to show we are running 
   pinMode(LED_PIN, OUTPUT);
@@ -164,28 +138,11 @@ void setup()
 
 void loop()
 {
-  if((laserOn == true) && (watingOnScan == false))
-  {
-    // Move the LIDAR servo
-    // The HiTEC HS-422 servo works in the left hand rule not the right hand rule used by ros.
-    // That is the HS-422 increases then angle in an anti-clockwise direction.
-    if(incDir == true)
-    { 
-      servoLidar.write(180-scanIndex);      
-    }
-    else
-    {
-      servoLidar.write(scanIndex);      
-    }
-
-    // Delay to give time for servo to move into position
-    scanTime = millis() + 10; //+ 3;
-    watingOnScan = true;
-  }
-
   // Is it time to publish the tacho message
   if(millis() > publisherTime)
   {
+    float deltaTime;
+    
     currentTime = micros();
     deltaTime = (float)(currentTime - lastTime)/1000000.0;
 
@@ -201,93 +158,6 @@ void loop()
     
     tachoPub.publish(&tachoMsg);
     publisherTime = millis() + 50; // Publish at 20Hz
-  }
-
-  // Is the LIDAR enabled
-  if((laserOn == true) && (millis() > scanTime))
-  {
-    if(incDir == true)
-    {
-      if(newScan == true)
-      {
-        // Moving anti-clockwise around Z, angle increasing with ROS standard
-        lidarMsg.angle_min = TRUE_MIN_ANGLE;
-        lidarMsg.angle_max = TRUE_MAX_ANGLE;
-        lidarMsg.angle_increment = ANGLE_INCREMENT;
-
-        // Time is acquisition time of first ray in scan
-        lidarMsg.header.stamp = nh.now();
-  
-        newScan = false;
-      }
-
-      // Read the range from the LIDAR
-      // Receiver bias correction must be performed periodically e.g. 1 out of every 100
-      if((scanIndex%100) == 0)
-      {
-        ranges[scanIndex] = (float)myLidarLite.distance() / 100.0f;
-      }
-      else
-      {
-        ranges[scanIndex] = (float)myLidarLite.distance(false) / 100.0f;
-      }
-
-      scanIndex++;
-
-      if(scanIndex == NUM_READINGS)
-      {
-        // End of the scan sequence
-        lidarMsg.ranges = ranges;  
-        lidarPub.publish(&lidarMsg);
-
-        // Setup to move in the other direction
-        scanIndex = 0;
-        newScan = true;
-        incDir = false;
-      }
-    }
-    else
-    {
-      if(newScan == true)
-      {
-        // Moving clockwise around Z, angle decreasing with ROS standard
-        lidarMsg.angle_min = TRUE_MAX_ANGLE;
-        lidarMsg.angle_max = TRUE_MIN_ANGLE;
-        lidarMsg.angle_increment = -ANGLE_INCREMENT;
-
-        // Time is acquisition time of first ray in scan
-        lidarMsg.header.stamp = nh.now();
-  
-        newScan = false;
-      } 
-
-      // Read the range from the LIDAR
-      // Receiver bias correction must be performed periodically e.g. 1 out of every 100
-      if((scanIndex%100) == 0)
-      {
-        ranges[scanIndex] = (float)myLidarLite.distance() / 100.0f;
-      }
-      else
-      {
-        ranges[scanIndex] = (float)myLidarLite.distance(false) / 100.0f;
-      }
-
-      scanIndex++;
-
-      if(scanIndex == NUM_READINGS)
-      {
-        // End of the scan sequence
-        lidarMsg.ranges = ranges;  
-        lidarPub.publish(&lidarMsg);
-
-        // Setup to move in the other direction
-        scanIndex = 0;
-        newScan = true;
-        incDir = true;
-      }      
-    }
-
-    watingOnScan = false;
   }
   
   nh.spinOnce();
@@ -320,30 +190,6 @@ void servo_cb( const servo_msgs::servo_array& cmd_msg)
       nh.logdebug("Error incorrect servo index");
       break;
   }
-}
-
-
-// Callback for when lidar enable command message is received
-void command_lidar_cb( const std_msgs::Bool& cmd_msg)
-{
-  // Turn the LIDAR scan on or off
-  laserOn = cmd_msg.data;
-
-  if(laserOn == true)
-  {
-    // Starting the scan from RC 0 degrees and increasing
-    scanIndex = 0;
-    newScan = true;
-    incDir = true;
-  }
-  else
-  {
-    // Set servo to start of scan for when next enabled
-    // Start at 0 degrees (-1.571  radians for ROS)
-    // Except the HiTEC HS-422 servo works in the left hand rule not 
-    // the right hand rule used by ros. So set 180 degrees for thsi type of servo
-    servoLidar.write(180);
-  }    
 }
 
 
